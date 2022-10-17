e0c9dde20140919410a103c6fc07f3a842df89dd
AhmadinZaki <ahmadin.zaki@gmail.com>
2022-10-18 00:53:20 +0700

    Nav & Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="container">
+            <nav className="mb-3 fixed-bottom " style={{ backgroundColor: "#A0B4F2"}}>
+                    <p className="text-center h3 " >
+                        FOOTER
+                    </p>
+            </nav>
+
+
+        </div >
+    )
+};
+
+export default Footer
--- src/components/NavApp.js
+++ src/components/NavApp.js
@@ -1,7 +1,24 @@
 import React from "react";
+import logo from '../assets/img/app-logo.svg'
 
 function NavApp() {
-  return <div>NavApp</div>;
-}
+  return (
+    <div className="container">
+      <div className="wrapper ">
+        <nav className="navbar navbar-expand-lg mt-3 fixed-top " style={{ backgroundColor: "#A0B4F2" }}>
+          <div className="container-fluid ">
+            <a className="navbar-brand h1 mx-4" href="#">
+              <img src={logo} alt="Logo" className="" width="50" height="30" />
+              Accounting
+            </a>
+          </div>
+          <form className="d-flex mx-4" role="search">
+            <button className="btn btn-danger btn-sm" type="submit">LOGOUT</button>
+          </form>
+        </nav>
+      </div >
+    </div >
+  )
+};
 
 export default NavApp;
