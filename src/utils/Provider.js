2254309df79f2e60fe242c686e056a29227d1ada
Rafly Pradana Putra <raflypradana31@gmail.com>
2022-10-17 16:57:49 +0700

    Update base url and change axios to async await

--- src/utils/Provider.js
+++ src/utils/Provider.js
@@ -1,1 +1,47 @@
 import { BASE_URL } from "./Helper";
+import axios from "axios";
+
+const getAllPerkiraan = async () => {
+  try {
+    let response = await axios.get(`${BASE_URL}`);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getPerkiraanById = async (id) => {
+  try {
+    let response = await axios.get(`${BASE_URL}/${id}`);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createPerkiraan = async (data) => {
+  try {
+    let response = await axios.post(`${BASE_URL}/`);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updatePerkiraan = async (data) => {
+  try {
+    let response = await axios.put(`${BASE_URL}`);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deletePerkiraan = async (data) => {
+  try {
+    let response = await axios.delete(`${BASE_URL}`);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
