ae9e8cf0d6824f33f1ba2f4987d1f19dd89e2313
AhmadinZaki <ahmadin.zaki@gmail.com>
2022-10-18 17:26:00 +0700

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { useState } from 'react';
+
+import logo from "../assets/img/app-logo.svg";
+
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+
+  return (<div>
+    <nav class="navbar">
+      <div className="container-fluid mt-3">
+        <div className="navbar-brand mx-4 h1 flex" href="#">
+          <img src={logo} alt="Logo" className="" />
+          Accounting
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="container-fluid mt-5" style={{ textAlign: "center" }}>
+        <h3 type="button" className="btn  mb-4 " style={{ color: "#4D3FA6" }}>Dashboard</h3>
+
+        <button type="button" className="btn  mb-4  " style={{ color: "#4D3FA6" }}>Perkiraan</button>
+
+        <button type="button" className="btn  mb-4  " style={{ color: "#4D3FA6" }}>Jurnal Umum</button>
+
+        <button type="button" className="btn " style={{ color: "#4D3FA6" }}> Jurnal Besar</button>
+
+      </div>
+    </div>
+  </div>
+  );
 }
 
 export default Sidebar;
