5f26dc53ecdbf4c24d322b2cc39a7e6b8a39a844
Rafly Pradana Putra <raflypradana31@gmail.com>
2022-10-18 16:02:59 +0700

    Modified project structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BaseLayout from "./pages/BaseLayout";
 import DashboardPage from "./pages/DashboardPage";
+import JurnalUmumPage from "./pages/JurnalUmumPage";
 import PerkiraanPage from "./pages/PerkiraanPage";
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
         <Route element={<BaseLayout />}>
           <Route index element={<DashboardPage />} />
           <Route path="/perkiraan" element={<PerkiraanPage />} />
+          <Route path="/jurnal-umum" element={<JurnalUmumPage />} />
+          <Route path="/jurnal-umum/create" element={<JurnalUmumPage />} />
+          <Route path="/jurnal-umum/edit" element={<JurnalUmumPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
