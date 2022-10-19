258a3fd0982e640efc99f64b49a60c6b0c7557fa
Rafly Pradana Putra <raflypradana31@gmail.com>
2022-10-19 13:39:15 +0700

    Remove unused import

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,31 +1,37 @@
 import React from "react";
-import { useState } from 'react';
-import "../css/style.css"
-import { Outlet, Link } from "react-router-dom";
+import "../css/style.css";
+import { Link } from "react-router-dom";
 import logo from "../assets/img/app-logo.svg";
 
-
 function Sidebar() {
-
-  return (<div>
-    <nav class="navbar">
-      <div className="container-fluid mt-3">
-        <div className="navbar-brand mx-4 h1 flex" href="#">
-          <img src={logo} alt="Logo" className="" />
-          Accounting
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="container-fluid mt-3">
+          <div className="navbar-brand mx-4 h1 flex" href="#">
+            <img src={logo} alt="Logo" className="" />
+            Accounting
+          </div>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="row my-4 mx-4">
+          <Link className="h6 side-hover text-center py-2" to="/">
+            Dashboard
+          </Link>
+          <Link className="h6 side-hover text-center py-2" to="/perkiraan">
+            Perkiraan
+          </Link>
+          <Link className="h6 side-hover text-center py-2" to="/jurnal-umum">
+            Jurnal Umum
+          </Link>
+          <Link className="h6 side-hover text-center py-2" to="/jurnal-besar">
+            {" "}
+            Jurnal Besar
+          </Link>
         </div>
       </div>
-    </nav>
-    <div className="container">
-      <div className="row my-4 mx-4">
-        <Link className="h6 side-hover text-center py-2" to="/">Dashboard</Link>
-        <Link className="h6 side-hover text-center py-2" to="/perkiraan">Perkiraan</Link>
-        <Link className="h6 side-hover text-center py-2" to="/jurnal-umum">Jurnal Umum</Link>
-        <Link className="h6 side-hover text-center py-2" to="/jurnal-besar"> Jurnal Besar</Link>
-
-      </div>
     </div>
-  </div>
   );
 }
 
